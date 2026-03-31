6defb1be44bf5fbd7f1f262f8ad4393a4807c632
Priyanshu Tiwari <official.priyanshu.37@gmail.com>
2026-03-31 19:45:19 +0530

    create user registration

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,15 +1,64 @@
 import {asynchandler} from "../utils/asynchandler.js"
-
-
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.models.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
 // Method to register user
 
 const registerUser = asynchandler(async (req,res)=> {
+    // get user details from frontend.
+   //  validation--> not empty.
+  //   check if user already exists or not--> can be chked thru username or email.
+ //    check for images and chk for avatars...if available upload them on cloudinary.
+/*     To chk whether avatar is uploade successfully on cloudinary , then create user object--> create entry in db.....
+       remove password nd refresh token from response.
+       chk whether response come or not , and if no response come chk user created or not and if not throw error....*/
 
- return res.status(200).json({
-    message: "ok"
-  })
+
+//--> To get user details-->
+      const {fullName,email,username,password} = req.body
+      console.log("email:", email);
+
+    /* if(fullName === ""){
+        throw new ApiError(400, "fullname is required")
+      }
+
+*/
+// checking all fields at once.
+    if(
+      [fullName,email,username,password].some((field) => field?.trim() === "")
+    ){
+      throw new ApiError(400, "All field are compulsory")
+    }
+
+// to chk user already exist or not:_>
+    const existedUser = User.findOne({
+      $or: [{ username },{ email }]
+
+    })
+
+    if(existedUser){
+      throw new ApiError(409, "User with given username or email already exist.")
+    }
+    console.log(existedUser);
+
+// to check images  // here ? means taking optionally
+   const avatarLocalPath = req.files?.avatar[0]?.path
+
+   const coverLocalPath = req.files?.avatar[0]?.path
+
+   if(!avatarLocalPath){
+    throw new ApiError(400, "Avatar file is required")
+   }
+
+
+//Uploading avatar and coverimage on cloudinary
+
+
+
 
 })
+
+
 
 
 export {registerUser, }
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
 
 
 
-const upload = multer({ storage, })
+export const upload = multer({ storage, })
 
 
 
